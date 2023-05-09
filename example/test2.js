86d68c43d7b651769b03d4b3f587aa9c3c305886
damartripamungkas <damartripamungkas@github.com>
2023-05-09 11:12:21 +0700

    docs: add and refactor example test

--- example/test2.js
+++ example/test2.js
@@ -1,7 +1,7 @@
 /**
  * test logger with write to file
  */
-const init = require('../index');
+const init = require("logging-pretty");
 const log = init("./test/test2-db.log"); // path your file
 log.info("this is information message log");
 log.warn("warn message lorem ipsum");
@@ -9,5 +9,6 @@ log.error("oops!! found error in function getData");
 log.debug("debugging function data start from 1 ~ 5");
 log.trace("tracing function data");
 log.fatal("unhandleError!! found error in function getData");
+log.custom("Feature", "this log with default color");
 log.custom("LOREM", "Lorem Ipsum is simply dummy text of the printing and typesetting industry", log.listColor.bold, log.listColor.whiteBright);
 log.custom("Feature CronJob", "syncron data to database success with count data 200", log.listColor.bold, log.listColor.whiteBright);
---
+++ example/test3.js
@@ -0,0 +1,14 @@
+/**
+ * test logger with write to file and optional tagId
+ */
+const init = require("logging-pretty");
+const log = init("./test/test2-db.log", "SELECTION"); // path your file
+log.info("this is information message log");
+log.warn("warn message lorem ipsum");
+log.error("oops!! found error in function getData");
+log.debug("debugging function data start from 1 ~ 5");
+log.trace("tracing function data");
+log.fatal("unhandleError!! found error in function getData");
+log.custom("Feature", "this log with default color");
+log.custom("LOREM", "Lorem Ipsum is simply dummy text of the printing and typesetting industry", log.listColor.bold, log.listColor.whiteBright);
+log.custom("Feature CronJob", "syncron data to database success with count data 200", log.listColor.bold, log.listColor.whiteBright);
