c795775ce0d20d84fb8e4f28c2e35c1c0400dd45
damartripamungkas <damartripamungkas@github.com>
2023-12-13 11:28:06 +0700

    feat: add control for display console.log

--- src/log.ts
+++ src/log.ts
@@ -6,14 +6,16 @@ type TypeArgs = string | null | undefined;
  * @param pathFolderLog example "./db.log" if path dont have file, script will create and write new file
  * @returns object
  */
-const init = (pathFolderLog: TypeArgs, hashTag: TypeArgs) => {
+const init = (pathFolderLog: TypeArgs, hashTag: TypeArgs, enableConsole = true) => {
   const { red, green, yellow, cyan, blue, bgRed } = chalk;
   const isFoundPathFolderLog = pathFolderLog === null || pathFolderLog === undefined ? null : true;
   const renderLog = (tag: string, msg: string, chalkFunc: any, chalkFuncBackground: any) => {
     const time = getTimeNow();
     msg = hashTag === undefined || hashTag === null ? msg : `#${hashTag} ${msg}`;
     chalkFuncBackground = chalkFuncBackground === undefined ? msg : chalkFuncBackground(msg);
-    console.log(`[${time}] [${chalkFunc(tag)}] ${chalkFuncBackground}`);
+    if (enableConsole === true) {
+      console.log(`[${time}] [${chalkFunc(tag)}] ${chalkFuncBackground}`);
+    }
 
     if (isFoundPathFolderLog === true) {
       writeLogToFile(pathFolderLog, `[${time}] [${tag}] ${msg}`);
@@ -23,7 +25,7 @@ const init = (pathFolderLog: TypeArgs, hashTag: TypeArgs) => {
   return {
     info: (msg: string) => renderLog("INFO", msg, green, undefined),
     warn: (msg: string) => renderLog("WARN", msg, yellow, yellow),
-    error: (msg: string) => renderLog("ERRR", msg, red, red),
+    error: (msg: string) => renderLog("ERR", msg, red, red),
     success: (msg: string) => renderLog("SUCCESS", msg, green, green),
     debug: (msg: string) => renderLog("DEBUG", msg, cyan, cyan),
     trace: (msg: string) => renderLog("TRACE", msg, blue, blue),
