efb0ab716ad5611dd320bb051f36f312c7229eeb
damartripamungkas <damartripamungkas@github.com>
2023-12-31 13:38:01 +0700

    fix: change hookMid to mid for functional middleware

--- src/log.ts
+++ src/log.ts
@@ -1,31 +1,34 @@
 import chalk from "chalk"
 import dayjs from "dayjs"
 import { appendFile } from "node:fs/promises"
+
 type TypeArgs = string | null
+type TypeMid = (msg: string) => string
 
 const getTimeNow = () => {
   return dayjs().format("YYYY-MM-DD HH:mm:ss:SSS")
 }
 
 /**
- * @param pathFile example "./db.log" if path dont have file, script will create and write new file
- * @param uniqTag unique tag for each log, if this is set then the log output will start with this #....
- * @param force force mode, if "pathFile" is set but this is set to "console" it will not write to the log file.
- * @param hookMid returns a message via callback before doing console.log and appendFile
+ * @param pathFile [optional] example "./db.log" if path dont have file, script will create and write new file
+ * @param uniqTag [optional] unique tag for each log, if this is set then the log output will start with this #....
+ * @param force [optional] force mode, if "pathFile" is set but this is set to "console" it will not write to the log file.
+ * @param mid [optional] middleware before write to console and file, must be return string
  * @returns object
  */
-const init = (pathFile: TypeArgs, uniqTag?: TypeArgs, force?: "console" | "file" | "all", hookMid?: (msg: string) => void) => {
+const init = (pathFile?: TypeArgs, uniqTag?: TypeArgs, force?: "console" | "file" | "all", mid?: TypeMid) => {
   const { red, green, yellow, cyan, blue, bgRed } = chalk
   const isFoundPathFile = pathFile === null || pathFile === undefined ? false : true
   force = force ? force : isFoundPathFile ? "all" : "console"
-  hookMid = typeof hookMid == "function" ? hookMid : (msg: string) => msg
+  mid = mid === null || mid === undefined ? (msg: string) => msg : mid
 
   const renderLog = (tag: string, msg: string, colorUniqTag: any, colorMsg: any) => {
+    msg = mid(msg)
     msg = uniqTag === undefined || uniqTag === null ? msg : `#${uniqTag} ${msg}`
-    colorMsg = colorMsg === undefined ? msg : colorMsg(msg)
 
     const time = getTimeNow()
     if (force == "console" || force == "all") {
+      colorMsg = colorMsg === undefined ? msg : colorMsg(msg)
       console.log(`[${time}] [${colorUniqTag(tag)}] ${colorMsg}`)
     }
 
@@ -35,8 +38,6 @@ const init = (pathFile: TypeArgs, uniqTag?: TypeArgs, force?: "console" | "file"
         appendFile(pathFile, txtFile)
       }
     }
-
-    hookMid(txtFile)
   }
 
   return {
