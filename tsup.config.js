e35579b3f3f55467b0f2a145e2f462cc20ea363f
damartripamungkas <damartripamungkas@github.com>
2024-07-14 17:51:30 +0700

    chore: update compile setting

---
+++ tsup.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  outDir: `./dist/src`,
+  entry: [`./src/index.ts`],
+  format: [`cjs`, `esm`],
+  cjsInterop: true,
+  dts: true,
+  clean: true,
+  minify: true,
+});
